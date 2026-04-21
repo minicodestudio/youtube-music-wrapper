a6439690589aaf07ee6381b471ea692f1db74f3f
hyeminkim <hyeminkim@hybecorp.com>
2026-04-21 17:06:55 +0900

    feat: 창 닫으면 메뉴 바에서도 닫히도록 개선

--- YouTubeMusicWrapper/AppDelegate.swift
+++ YouTubeMusicWrapper/AppDelegate.swift
@@ -23,16 +23,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        false
-    }
-
-    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
-        if !flag {
-            for window in sender.windows {
-                window.makeKeyAndOrderFront(nil)
-            }
-        }
-        return true
+        true
     }
 }
 
